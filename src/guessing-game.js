45934b4b4e3b8ef4ee931a4d5f2fff916d50191a
ArtsiomDeribo <31903188+ArtsiomDeribo@users.noreply.github.com>
2017-09-13 21:12:40 +0300

    Add files via upload

---
+++ src/guessing-game.js
@@ -0,0 +1,27 @@
+class GuessingGame {
+    constructor() {
+        this.min;
+        this.max;
+        this.mid;
+    }
+
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    guess() {
+        this.mid = Math.ceil((this.max-this.min)/2);
+        return this.mid;
+    }
+
+    lower() {
+        this.max = this.mid;
+    }
+
+    greater() {
+        this.min = -this.mid;
+    }
+}
+
+module.exports = GuessingGame;
